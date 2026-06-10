0d4bccbbdcd3106a52408f4192ec3a1b9c655ef7
Shizla Fashia <shizla.fashia.an24@stu.pnj.ac.id>
2026-06-10 22:23:35 +0700

    Update db.php

--- db.php
+++ db.php
@@ -1,19 +1,1 @@
-<?php
-// Database configuration
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "youthreverfest_db";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-// Set charset to utf8mb4
-$conn->set_charset("utf8mb4");
-
-?>
